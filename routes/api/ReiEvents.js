0890dc9b6d2db3f0edf71a288d4f83cd9f706a6f
Jonathan Day <jon@jonuday.com>
2019-04-22 16:22:49 -0700

    Add branch with only REI calls.

--- routes/api/ReiEvents.js
+++ routes/api/ReiEvents.js
@@ -9,29 +9,16 @@ module.exports = (app) => {
         let category = req.query.category;
         let location = req.query.location;
         
-        let apiUrl = 'http://localhost:5000/temp/events'; 
-        if (process.env.NODE_ENV === 'production') {
-            apiUrl = 'https://stark-sea-90144.herokuapp.com/temp/events';
-        } 
-        if (process.env.REI_CALLS === 'true') {           
-            apiUrl = 'https://www.rei.com/events/a/' + category + '?previousLocation=' + encodeURIComponent(location) + '%2C+USA&course.session.anyLocation=100.000000~38.232417~-122.636652;geo_r'
-            console.log('Calling rei.com/events');
-        }
-        if (apiUrl != '') {
-            fetch(apiUrl)
-            .then(res => res.text())
-            .then(data => {
-                res.send({ events: data });
-            })
-            .catch(err => {
-                res.redirect('/error');
-            })
-        } 
-        else {
-            // temporary response
-            res.send({ events: [
-                {id: 1, title: "Events Test Title"}
-            ] });
-        }
+        let apiUrl = 'https://www.rei.com/events/a/' + category + '?previousLocation=' + encodeURIComponent(location) + '%2C+USA&course.session.anyLocation=100.000000~38.232417~-122.636652;geo_r'
+        console.log('Calling rei.com/events');
+
+        fetch(apiUrl)
+        .then(res => res.text())
+        .then(data => {
+            res.send({ events: data });
+        })
+        .catch(err => {
+            res.redirect('/error');
+        })
     });
 }
